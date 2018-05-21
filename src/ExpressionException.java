7281916f545f27e1b91c8328b983815e357411b7
WangJieCS <hncs_wj@163.com>
2018-05-21 15:37:23 +0800

    Initial commit

---
+++ src/ExpressionException.java
@@ -0,0 +1,20 @@
+public class ExpressionException extends RuntimeException{  
+  
+    private static final long serialVersionUID = 1L;  
+  
+    public ExpressionException() {  
+        super();  
+    }  
+  
+    public ExpressionException(String msg) {  
+        super(msg);  
+    }  
+      
+    public ExpressionException(String msg, Throwable cause) {  
+        super(msg,cause);  
+    }  
+      
+    public ExpressionException(Throwable cause) {  
+        super(cause);  
+    }  
+}  
---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+public class Test2 {  
+      
+    public static void main(String[] args){  
+          
+       
+    	String   resultStr ="sqrt(4)*3-5*(6-2)*cbrt(8)*(6-2)";
+    	System.out.println(resultStr);
+        System.out.println(ExpressionEvaluator.CalcExpression(ExpressionEvaluator.parse(resultStr)));  
+        
+    	resultStr ="-3+sqrt(4)*2-4";
+    	System.out.println(resultStr);
+        System.out.println(ExpressionEvaluator.CalcExpression(ExpressionEvaluator.parse(resultStr)));  
+        
+        resultStr ="A+sqrt(x)*2-4.2";
+        System.out.println(resultStr);
+        ExpressionEvaluator.parse(resultStr);
+    }  
+}  
